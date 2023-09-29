cce22270922302821fb264ba990ebb904c2cc360
CallumD21 <callum.dickinson176@gmail.com>
2023-09-29 15:35:32 +0100

    Completed react project createing a site with a h1 and an unordered list

---
+++ React/jsx-code-challenge/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My most recently watched films</h1>
+    <ul>
+      <li>Blue Beatle</li>
+      <li>Tenet</li>
+      <li>Oppenheimer</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
