696dcdacc17845e2687dfcd6c058ab0aa4b3cbf0
CallumD21 <callum.dickinson176@gmail.com>
2023-09-29 17:08:31 +0100

    Created new React project using JSX attributes such as className

---
+++ React/5-jsx-attributes-and-styling/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const randomImage = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <div>
+      <img alt="Random" src={randomImage} />
+      <img alt="Bacon" src="https://www.wellplated.com/wp-content/uploads/2023/05/Oven-Baked-Bacon.jpg" />
+      <img alt="Jamon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Jam%C3%B3n_de_Guijuelo_004_%28cropped%29_4.3.JPG/800px-Jam%C3%B3n_de_Guijuelo_004_%28cropped%29_4.3.JPG" />
+      <img alt="Noodles" src="https://www.whiskaffair.com/wp-content/uploads/2021/05/Gochujang-Noodles-2-3.jpg" />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
