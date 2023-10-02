a994e69e1284fa163786d8bda876b5098bb0bd6a
CallumD21 <callum.dickinson176@gmail.com>
2023-10-02 14:52:50 +0100

    Added Header, Note and Footer components to the Keeper project

---
+++ React/9-keeper-app-part-1/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    return (
+        <footer>
+            <p>Copyright ⓒ {currentYear}</p>
+        </footer>
+    );
+}
+
+export default Footer;
