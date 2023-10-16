83bc3f95b369f77fc458df7f90bbddc689a5cce1
CallumD21 <callum.dickinson176@gmail.com>
2023-10-16 16:50:29 +0100

    On click of the list item toggle a line through style

--- React/18-component-tree/src/components/ToDoItem.jsx
+++ React/18-component-tree/src/components/ToDoItem.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function ToDoItem(props) {
-    return (<li>{props.item}</li>);
+    const [lineThrough, setLineThrough] = useState(false);
+
+    function toggleLineThrough() {
+        setLineThrough(prevValue => !prevValue);
+    }
+
+    const listItemStyle = {
+        textDecoration: lineThrough ? "line-through" : "none"
+    };
+
+    return (
+        <li style={listItemStyle} onClick={toggleLineThrough}>{props.item}</li>
+    );
 }
 
 export default ToDoItem;
