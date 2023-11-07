76a06ea0baab29bbc3d3d2c784ad1198111bcf1f
CallumD21 <callum.dickinson176@gmail.com>
2023-11-07 20:07:06 +0000

    Implemented the add note functionality

--- React/19-keeper-app-part-3/src/components/App.jsx
+++ React/19-keeper-app-part-3/src/components/App.jsx
@@ -4,24 +4,30 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-function createNote(note) {
-    return (<Note key={note.id} title={note.title} content={note.content} />)
+function createNote(note, index) {
+    return (<Note key={index} title={note.title} content={note.content} />)
 }
 
 function App() {
-    let initialState = [
-        {
-            id: 1,
-            title: "Note title",
-            content: "Note content"
-        }
-    ];
-    const [notes, setNotes] = useState(initialState);
+    const [notes, setNotes] = useState([]);
+
+    function addNote(title, content) {
+        const newNote = {
+            title: title,
+            content: content
+        };
+
+        setNotes(prevValue => {
+            let newNotes = [...prevValue, newNote];
+
+            return newNotes;
+        });
+    }
 
     return (
         <div>
             <Header />
-            <CreateArea />
+            <CreateArea addNote={addNote} />
             {notes.map(createNote)}
             <Footer />
         </div>
--- React/19-keeper-app-part-3/src/components/CreateArea.jsx
+++ React/19-keeper-app-part-3/src/components/CreateArea.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    function updateTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function updateContent(event) {
+        setContent(event.target.value);
+    }
+
+    function onSubmit(event) {
+        event.preventDefault();
+        props.addNote(title, content);
+
+        //Clear data for next entry
+        setTitle("");
+        setContent("");
+    }
+
     return (
         <div>
-            <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
+            <form onSubmit={onSubmit}>
+                <input name="title" placeholder="Title" value={title} onChange={updateTitle} />
+                <textarea name="content" placeholder="Take a note..." rows="3" value={content} onChange={updateContent} />
                 <button>Add</button>
             </form>
         </div>
