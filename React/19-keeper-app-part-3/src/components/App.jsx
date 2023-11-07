bb5214465dc75f282b839275c164254a8c8a4189
CallumD21 <callum.dickinson176@gmail.com>
2023-11-07 19:38:13 +0000

    Updated notes to be a list of objects and added a function to map to a Note component

--- React/19-keeper-app-part-3/src/components/App.jsx
+++ React/19-keeper-app-part-3/src/components/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+function createNote(note) {
+    return (<Note key={note.id} title={note.title} content={note.content} />)
+}
+
 function App() {
+    let initialState = [
+        {
+            id: 1,
+            title: "Note title",
+            content: "Note content"
+        }
+    ];
+    const [notes, setNotes] = useState(initialState);
+
     return (
         <div>
             <Header />
             <CreateArea />
-            <Note key={1} title="Note title" content="Note content" />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
