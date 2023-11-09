f6dbe6a0c0b0d51ce83af3af88988bf8d061127f
CallumD21 <callum.dickinson176@gmail.com>
2023-11-09 21:56:49 +0000

    Updated style sheet

--- React/20-prebuilt-react-components/src/components/CreateArea.jsx
+++ React/20-prebuilt-react-components/src/components/CreateArea.jsx
@@ -32,7 +32,7 @@ function CreateArea(props) {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
+            <form className="create-note" onSubmit={onSubmit}>
                 <input name="title" placeholder="Title" value={note.title} onChange={updateNote} />
                 <textarea name="content" placeholder="Take a note..." rows="3" value={note.content} onChange={updateNote} />
                 <Fab>
